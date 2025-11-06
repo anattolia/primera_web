7d75ebc9d073b39c660a6e49e626bfbd2a66c901
Antonia Bustamante <bustamanter.antonia@gmail.com>
2025-11-06 18:34:35 +0000

    js básico

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 const titulo = document.getElementById("titulo");
+const textoInicial = document.getElementById("textoInicial");
+let estadoInicial = true;
+let posicionTexto = 0;
+const textos = ["Hola", "Bienvenidx", "a", "esta", "página", "web"];
+
+textoInicial.innerText = textos[posicionTexto];
+
+textoInicial.addEventListener("click", () => {
+  if (posicionTexto < textos.length - 1) {
+  posicionTexto = posicionTexto + 1;
+  textoInicial.innerText = textos[posicionTexto];
+  } else {
+    posicionTexto = 0;
+    textoInicial.innerText = textos[posicionTexto];
+  }
+})
 
 titulo.addEventListener("click", () => {
-  titulo.innerText = "Bienvenidxs";
+  if (estadoInicial === true) {
+    titulo.innerText = "Bienvenidxs";
+  } else {
+    titulo.innerText = "Hola, soy Don Pepe Adivinador";
+  }
+  estadoInicial = !estadoInicial;
 })
